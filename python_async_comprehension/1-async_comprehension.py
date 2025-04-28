5ac50e9dbd68ee681ac62810a524f4855d3430ed
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-04-28 19:10:32 +0200

    oui

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,13 +4,14 @@ comment
 """
 
 
-import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     comment
     """
-    result = [x async for x in async_generator()]
+
+    result = [x async for x in async_generator() if x < 11]
     return result
