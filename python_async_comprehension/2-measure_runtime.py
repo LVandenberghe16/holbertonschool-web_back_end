f3bc0b33b88e9d6151e031a6c07afbf4d971613b
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-04-28 18:20:12 +0200

    oui

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,4 +12,12 @@ async def measure_runtime():
     """
     comment
     """
-    pass
+    start = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    end = time.perf_counter()
+    return end - start
