df9381ea700b897c15adfadae3fbc373630851fb
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-04-28 18:26:03 +0200

    oui

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,9 @@ comment
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
-
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     comment
     """
