e77e18dfa90b42432243792b0e415ba603d04a96
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-05-20 11:45:07 +0200

    oui

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -5,7 +5,8 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  const url = req.url;
+  const { url } = req;
+
   if (url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     countStudents(database)
@@ -13,7 +14,7 @@ const app = http.createServer((req, res) => {
         res.end(`This is the list of our students\n${output}`);
       })
       .catch((error) => {
-        res.end(error.message);
+        res.end(`This is the list of our students\n${error.message}`);
       });
   } else {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
