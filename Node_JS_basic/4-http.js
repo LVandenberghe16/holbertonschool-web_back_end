307cef384fe3cbaef53da6c478c61f07a8ea4bc1
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-05-20 11:19:08 +0200

    oui

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,10 @@
 const http = require('http');
 
-const port = process.env.PORT || 1245;
-
-http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  res.end('Hello Holberton School!');
 });
+
+app.listen(1245);
+
+module.exports = app;
