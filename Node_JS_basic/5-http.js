c23ea9e6fcb664615d41c1958c953c0a6b153bb7
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-05-20 11:39:37 +0200

    oui

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    countStudents(database)
+      .then((output) => {
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
